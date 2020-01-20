9025a950594a41dbf1903148db6e2ddb953a839b
Andrew <arkozak@alaska.edu>
2020-01-19 15:26:01 -0900

    created a temporary int main in temp.cpp that uses the user arguments to convert temp

--- Homework 0/temp.cpp
+++ Homework 0/temp.cpp
@@ -1,9 +1,24 @@
 #include <string>
 using std::strtod;
+#include <iostream>
+using std::cout; 
+using std::endl;
 
 double c_ctof(const char* str)
 {
 	double x = strtod(str, nullptr);
 
 	return x * (9.0 / 5.0) + 32.0;
+}
+
+double cpp_ftoc(const char* str)
+{
+	double x = strtod(str, nullptr);
+
+	return (x -32.0) * (5.0 / 9.0);
+}
+
+int main(int argc, char* argv[])
+{
+	cout << c_ctof(argv[1]);
 }
