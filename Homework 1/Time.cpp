3ea378570f660d6d96183e1ac74f5c1a7d98348c
Andrew <arkozak@alaska.edu>
2020-01-26 18:05:57 -0900

    created a way to start the timer in Time.cpp's Time class  in homework 1

--- Homework 1/Time.cpp
+++ Homework 1/Time.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+using std::cout;
 #include <chrono>
+using std::chrono::system_clock;
+using std::chrono::seconds;
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
 
 
 class Time
 {
 public:
-
+	void begin()
+	{
+		start = system_clock::now();
+	}
+	
 private:
-
+	system_clock::time_point start;
 };
+
