d78f439870e7234319d8c96484feded988a3beda
Andrew <arkozak@alaska.edu>
2020-01-26 18:06:38 -0900

    created a way to end the time in Time.cpp's Time class in Homework 1

--- Homework 1/Time.cpp
+++ Homework 1/Time.cpp
@@ -14,8 +14,14 @@ public:
 	{
 		start = system_clock::now();
 	}
-	
+	void finish()
+	{
+		end = system_clock::now();
+		cout << duration_cast<seconds>(end - start).count() << "sec\n";
+		cout << duration_cast<milliseconds>(end - start).count() << "ms\n";
+	}
 private:
 	system_clock::time_point start;
+	system_clock::time_point end;
 };
 
