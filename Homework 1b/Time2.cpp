b6e68abce06d9d27662c681db25329dd63446c06
Andrew <arkozak@alaska.edu>
2020-01-26 21:11:58 -0900

    Created the adding of the vectors in int main in Time2.cpp in Homework 1b project. Starting add2vector function

--- Homework 1b/Time2.cpp
+++ Homework 1b/Time2.cpp
@@ -6,8 +6,16 @@ using std::chrono::system_clock;
 using std::chrono::seconds;
 using std::chrono::milliseconds;
 using std::chrono::duration_cast;
-#include <random>
 #include <vector>
+using std::vector;
+#include <string>
+using std::string;
+using std::getline;
+#include <sstream>
+using std::istringstream;
+#include <fstream>
+using std::ifstream;
+#include <algorithm>
 
 
 class Time
@@ -33,3 +41,41 @@ private:
 	typedef std::chrono::duration<double> fsec;
 	typedef std::chrono::duration<double> fms;
 };
+
+
+
+
+
+int main()
+{
+	Time t;
+
+	cout << "Testing Vectors";
+
+	//Filling vector
+	vector<string> books;
+
+	cout << "The program is reading: Mrs Whittelsey's Magazine for Mothers and Daughters, Book Of Mormon, Hints For Lovers, The Kama Sutra, and Irish Fairy Tales." << endl;
+	Time t;
+	add2vec(books, "Mrs_Whittelsey's_Magazine_for_Mothers_and_Daughters.txt");
+	cout << "Added Mrs Whittelsey's Magazine for Mothers and Daughters to Vector." << endl;
+	add2vec(books, "Book_Of_Mormon.txt");
+	cout << "Added Book Of Mormon to Vector." << endl;
+	add2vec(books, "Hints_For_Lovers.txt");
+	cout << "Added Hints For Lovers to Vector." << endl;
+	add2vec(books, "Kama_Sutra.txt");
+	cout << "Added The Kama Sutra to Vector." << endl;
+	add2vec(books, "Irish_Fairy_Tales.txt");
+	cout << "Added Irish Fairy Tales to Vector." << endl;
+	t.finish();
+
+
+
+
+
+
+
+
+
+	return 0;
+}
