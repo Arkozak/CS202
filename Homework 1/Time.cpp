82f4f1922d24b063bc51edd5742c774b29827c93
Andrew <arkozak@alaska.edu>
2020-01-26 18:16:57 -0900

    tested Time.cpp in homework one and changed how time display works so now ms and sec are displayed as doubles

--- Homework 1/Time.cpp
+++ Homework 1/Time.cpp
@@ -10,6 +10,10 @@ using std::chrono::duration_cast;
 class Time
 {
 public:
+	Time()
+	{
+		begin();
+	}
 	void begin()
 	{
 		start = system_clock::now();
@@ -17,11 +21,31 @@ public:
 	void finish()
 	{
 		end = system_clock::now();
-		cout << duration_cast<seconds>(end - start).count() << "sec\n";
-		cout << duration_cast<milliseconds>(end - start).count() << "ms\n";
+		cout << duration_cast<fsec>(end - start).count() << "sec\n";
+		cout << duration_cast<fms>((end - start)*1000).count() << "ms\n";
 	}
 private:
 	system_clock::time_point start;
 	system_clock::time_point end;
+	typedef std::chrono::duration<double> fsec;
+	typedef std::chrono::duration<double> fms;
 };
 
+int main()
+{
+	
+	Time t;
+	int x;
+
+	t.begin();
+
+	std::cin >> x;
+
+	t.finish();
+
+	t.begin();
+
+	std::cin >> x;
+
+	t.finish();
+}
