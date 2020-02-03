c0cea1a59cf41c20d6951764083086e0cbe5c06f
Andrew <arkozak@alaska.edu>
2020-02-02 23:17:45 -0900

    Homework 2: value.hpp & value.cpp: Realizing my mistake I am making each function it's own thing in a header file casue there is not going to be an int main

---
+++ Homework 2/value.cpp
@@ -0,0 +1,36 @@
+#include "value.hpp"
+
+void queuepush(list<std::shared_ptr<Monster>> enemies, const Monster& goblin)
+{
+
+}
+
+void queuepop(list<std::shared_ptr<Monster>> enemies)
+{
+
+}
+
+void stackpush(list<std::shared_ptr<Monster>> enemies, const Monster& goblin)
+{
+
+}
+
+void stackpop(list<std::shared_ptr<Monster>> enemies)
+{
+
+}
+
+void insert(list<std::shared_ptr<Monster>> enemies, const Monster& goblin)
+{
+
+}
+
+void find(list<std::shared_ptr<Monster>> enemies, const Monster& goblin)
+{
+
+}
+
+void printenemies(list<std::shared_ptr<Monster>> enemies)
+{
+
+}
---
+++ Homework 2/value.hpp
@@ -0,0 +1,41 @@
+#ifndef VALUE_HPP_
+#define VALUE_HPP_
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <list>
+using std::list;
+#include <memory>
+using std::make_shared;
+using std::shared_ptr;
+#include <algorithm>
+using std::find;
+
+struct Monster
+{
+	std::string Name;
+	int STR = 0;
+	int DEX = 0;
+	int CON = 0;
+	int INT = 0;
+	int WIS = 0;
+	int CHA = 0;
+};
+
+void queuepush(list<std::shared_ptr<Monster>> enemies, const Monster& goblin);
+
+void queuepop(list<std::shared_ptr<Monster>> enemies);
+
+void stackpush(list<std::shared_ptr<Monster>> enemies, const Monster& goblin);
+
+void stackpop(list<std::shared_ptr<Monster>> enemies);
+
+void insert(list<std::shared_ptr<Monster>> enemies, const Monster& goblin);
+
+void find(list<std::shared_ptr<Monster>> enemies, const Monster& goblin);
+
+void printenemies(list<std::shared_ptr<Monster>> enemies);
+
+
+#endif
