daff5ff4bee05541d1ae673f939f9bf7c701e9b0
Andrew Kozak <arkozak@alaska.edu>
2020-02-04 10:53:32 -0900

    created a function in lab 4 to read the file

--- Lab 4/Lab 4/Streams.cpp
+++ Lab 4/Lab 4/Streams.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include <fstream>
 
+void readfile()
+{
+	std::fstream doc;
+	std::ofstream("jumpsuites.txt");
+	std::string word;
+
+	while (doc >> word)
+	{
+		std::cout << word << std::endl;
+	}
+}
+
 int main()
 {
 	std::ifstream("jumpsuites.txt");
