a0fb2d93284f1d6fe4d84cd1b76b6872a6a3becc
Andrew <arkozak@alaska.edu>
2020-04-07 20:50:10 -0800

    added timer

--- Iditarod 2/main.cpp
+++ Iditarod 2/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <chrono>
+using std::chrono::system_clock;
+using std::chrono::seconds;
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
 
+//test
+class Time
+{
+public:
+	Time()
+	{
+		begin();
+	}
+	void begin()
+	{
+		start = system_clock::now();
+	}
+	void finish()
+	{
+		end = system_clock::now();
+		cout << duration_cast<fsec>(end - start).count() << "sec\n";
+		cout << duration_cast<fms>((end - start) * 1000).count() << "ms\n";
+	}
+private:
+	system_clock::time_point start;
+	system_clock::time_point end;
+	typedef std::chrono::duration<double> fsec;
+	typedef std::chrono::duration<double> fms;
+};
 
 int ack(int n, int j)
 {
@@ -9,21 +38,25 @@ int ack(int n, int j)
 	{
 		return j + 1;
 	}
-	if ((n > 0) && (j == 0))
+	else if ((n > 0) && (j == 0))
 	{
 		return ack(n - 1, 1);
 	}
-	if ((n > 0) && (j > 0))
+	else if ((n > 0) && (j > 0))
 	{
 		return ack(n - 1, ack(n, j - 1));
 	}
+	return 0;
 }
 
 
 int main()
 {
-	cout << ack(4, 7);
-
+	int x = 1;
+	int y = 5;
+	Time t;
+	cout << ack(x, y) << endl;
+	t.finish();
 
 	return 0;
 }
