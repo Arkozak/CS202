4339d0c1aa248c3e05a832e398ba238dddc6b308
Andrew <arkozak@alaska.edu>
2020-04-07 20:40:52 -0800

    Created ackermanns function

--- Iditarod 2/main.cpp
+++ Iditarod 2/main.cpp
@@ -3,12 +3,26 @@ using std::cout;
 using std::endl;
 
 
-
+int ack(int n, int j)
+{
+	if (n == 0)
+	{
+		return j + 1;
+	}
+	if ((n > 0) && (j == 0))
+	{
+		return ack(n - 1, 1);
+	}
+	if ((n > 0) && (j > 0))
+	{
+		return ack(n - 1, ack(n, j - 1));
+	}
+}
 
 
 int main()
 {
-
+	cout << ack(4, 7);
 
 
 	return 0;
