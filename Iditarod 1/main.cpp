c38871f5873e847dcda9c6e5525e815befb8738c
Andrew <arkozak@alaska.edu>
2020-04-07 18:56:54 -0800

    created the fibonacci sequesnce with recursion

--- Iditarod 1/main.cpp
+++ Iditarod 1/main.cpp
@@ -3,7 +3,14 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-
+int fib(int n)
+{
+	if (n <= 1)
+	{
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
 
 
 
@@ -16,11 +23,9 @@ using std::endl;
 
 int main()
 {
+	int x = 7;
 
-
-
-
-
+	cout << "The 7th number in the fibonaci sequence is " << fib(7);
 
 
 
