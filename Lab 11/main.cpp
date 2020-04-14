53a3b30722e97cac60e898afed26e0ab7d894e4b
Andrew <arkozak@alaska.edu>
2020-04-13 23:28:39 -0800

    added derived class to main.cpp in lab 11

--- Lab 11/main.cpp
+++ Lab 11/main.cpp
@@ -7,7 +7,8 @@ using std::string;
 class Base
 {
 public:
-	Base() {
+	Base() 
+	{
 		cout << "Constructing Base object with initial value of:  " << initial << endl;
 	}
 
@@ -24,12 +25,40 @@ private:
 	int initial = 20;
 };
 
+class Derived : public Base 
+{
+public:
+	Derived(): Base(1980), movie("Original IT")
+	{
+		cout << "Constructing a Derived object with movie title: " << movie << endl;
+	}
+
+	Derived(int initial, const std::string& title): Base(initial), movie(title)
+	{
+		cout << "Constructing a Derived object with movie title: " << movie << endl;
+	}
+
+	~Derived() 
+	{
+		cout << "Destructing a Derived object with movie title: " << movie << endl;
+	}
+
+private:
+	string movie;
+};
+
 int main()
 {
 	Base a; 
 	cout << endl;
 
 	Base b(2); 
+	cout << endl;
+
+	Derived c;
+	cout << endl;
+
+	Derived d (2017, "New IT"); 
 	cout << endl;
 
 	return 0;
