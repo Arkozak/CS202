61438fea760697375ede75bedd33c4351fcb30cf
Andrew <arkozak@alaska.edu>
2020-04-14 21:52:08 -0800

    created iditarod 3 with the 5 main files needed

---
+++ Iditarod 3/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_HPP_
+#define LISTS_HPP_
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include "nodes.hpp"
+#include <cmath>
+
+class CityList
+{
+public:
+private:
+};
+
+#endif
---
+++ Iditarod 3/nodes.hpp
@@ -0,0 +1,13 @@
+#ifndef NODES_HPP_
+#define NODES_HPP_
+
+#include <string>
+using std::string;
+
+class CityNode
+{
+public:
+private:
+};
+
+#endif
