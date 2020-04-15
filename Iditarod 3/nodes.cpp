b6ff251269eed77a4aed8b57f451a5738fea1d54
Andrew <arkozak@alaska.edu>
2020-04-14 21:59:37 -0800

    created constuctors for CityNode class along with functions to set the node values

--- Iditarod 3/nodes.cpp
+++ Iditarod 3/nodes.cpp
@@ -1,2 +1,23 @@
 #include "nodes.hpp"
 
+//base constructor
+CityNode::CityNode() {}
+
+//constructor with perameters
+CityNode::CityNode(unsigned int node, double latY, double lonX)
+{
+	nodeNumber = node;
+	latitude = latY;
+	longitude = lonX;
+}
+
+//setting the node
+void CityNode::setNode(unsigned int node, double lat, double lon)
+{
+	nodeNumber = node;
+	latitude = lat;
+	longitude = lon;
+}
+
+
+
--- Iditarod 3/nodes.hpp
+++ Iditarod 3/nodes.hpp
@@ -7,7 +7,14 @@ using std::string;
 class CityNode
 {
 public:
+	CityNode();
+	CityNode(unsigned int node, double latY, double lonX);
+	void setNode(unsigned int node, double lat, double lon);
+
 private:
+	unsigned int nodeNumber;
+	double latitude;
+	double longitude;
 };
 
 #endif
